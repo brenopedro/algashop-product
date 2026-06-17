c59086df6d588c8cf3700679aff67e28c7956e31
Breno Pedro <brenopedro14@hotmail.com>
2026-06-17 18:47:51 -0300

    feat: domain de produto

---
+++ src/main/java/com/algaworks/algashop/product/catalog/domain/model/product/Product.java
@@ -0,0 +1,57 @@
+package com.algaworks.algashop.product.catalog.domain.model.product;
+
+
+import com.algaworks.algashop.product.catalog.domain.model.IdGenerator;
+import lombok.*;
+import org.springframework.data.annotation.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Getter
+@Document(collection = "products")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Product {
+
+    @Id
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    private String brand;
+    private String name;
+    private String description;
+    private BigDecimal regularPrice;
+    private BigDecimal salePrice;
+    private Integer quantityInStock;
+    private Boolean enabled;
+
+    @Version
+    private Long version;
+
+    @CreatedDate
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    private OffsetDateTime updatedAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
+
+    @Builder
+    public Product(String brand, String name, String description, BigDecimal regularPrice,
+                   BigDecimal salePrice, Boolean enabled) {
+        this.id = IdGenerator.generateTimeBasedUUID();
+        this.brand = brand;
+        this.name = name;
+        this.description = description;
+        this.regularPrice = regularPrice;
+        this.salePrice = salePrice;
+        this.enabled = enabled;
+    }
+}
