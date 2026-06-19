a8f7219eb23a6635b73c136c331e446b2bcb4f3d
Breno Pinto <brenopedro14@hotmail.com>
2026-06-19 11:55:28 -0300

    feat: filtro de produtos para preço e data

--- src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
+++ src/main/java/com/algaworks/algashop/product/catalog/infrastructure/persistence/product/ProductQueryServiceImpl.java
@@ -1,24 +1,30 @@
 package com.algaworks.algashop.product.catalog.infrastructure.persistence.product;
 
-import java.util.UUID;
-
 import com.algaworks.algashop.product.catalog.application.product.query.*;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.stereotype.Service;
-
 import com.algaworks.algashop.product.catalog.application.utility.Mapper;
 import com.algaworks.algashop.product.catalog.domain.model.product.Product;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductNotFoundException;
 import com.algaworks.algashop.product.catalog.domain.model.product.ProductRepository;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.aggregation.AggregationExpressionCriteria;
+import org.springframework.data.mongodb.core.aggregation.ComparisonOperators;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ProductQueryServiceImpl implements ProductQueryService {
 
     private final ProductRepository productRepository;
+    private final MongoOperations mongoOperations;
     private final Mapper mapper;
 
     @Override
@@ -30,8 +36,79 @@ public class ProductQueryServiceImpl implements ProductQueryService {
 
     @Override
     public PageModel<ProductSummaryOutput> filter(ProductFilter filter) {
-        Page<Product> products = productRepository.findAll(PageRequest.of(filter.getPage(), filter.getSize()));
-        Page<ProductSummaryOutput> productOutputs = products.map(p -> mapper.convert(p, ProductSummaryOutput.class));
-        return PageModel.of(productOutputs);
+        Query query = queryWith(filter);
+        long totalItems = mongoOperations.count(query, Product.class);
+        Sort sort = sortWith(filter);
+
+        PageRequest pageRequest = PageRequest.of(filter.getPage(), filter.getSize(), sort);
+        Query pagedQuery = queryWith(filter).with(pageRequest);
+
+        List<Product> products;
+        int totalPages = 0;
+        if (totalItems > 0) {
+            products = mongoOperations.find(pagedQuery, Product.class);
+            totalPages = (int) Math.ceil((double) totalItems / pageRequest.getPageSize());
+        } else
+            products = new ArrayList<>();
+
+        List<ProductSummaryOutput> productOutput = products.stream()
+                .map(p -> mapper.convert(p, ProductSummaryOutput.class))
+                .toList();
+
+        return PageModel.<ProductSummaryOutput>builder()
+                .content(productOutput)
+                .number(pageRequest.getPageNumber())
+                .size(pageRequest.getPageSize())
+                .totalElements(totalItems)
+                .totalPages(totalPages)
+                .build();
+    }
+
+    private Sort sortWith(ProductFilter filter) {
+        return Sort.by(filter.getSortDirectionOrDefault(), filter.getSortByPropertyOrDefault().getPropertyName());
+    }
+
+    private Query queryWith(ProductFilter filter) {
+        Query query = new Query();
+        if (filter.getEnabled() != null)
+            query.addCriteria(Criteria.where("enabled").is(filter.getEnabled()));
+
+        if (filter.getAddedAtFrom() != null && filter.getAddedAtTo() != null) {
+            query.addCriteria(Criteria.where("addedAt")
+                    .gte(filter.getAddedAtFrom())
+                    .lte(filter.getAddedAtTo()));
+        } else {
+            if (filter.getAddedAtFrom() != null)
+                query.addCriteria(Criteria.where("addedAt").gte(filter.getAddedAtFrom()));
+            else if (filter.getAddedAtTo() != null)
+                query.addCriteria(Criteria.where("addedAt").lte(filter.getAddedAtTo()));
+        }
+
+        if (filter.getPriceFrom() != null && filter.getPriceTo() != null) {
+            query.addCriteria(Criteria.where("salePrice")
+                    .gte(filter.getPriceFrom())
+                    .lte(filter.getPriceTo()));
+        } else {
+            if (filter.getPriceFrom() != null)
+                query.addCriteria(Criteria.where("salePrice").gte(filter.getAddedAtFrom()));
+            else if (filter.getPriceTo() != null)
+                query.addCriteria(Criteria.where("salePrice").lte(filter.getPriceTo()));
+        }
+
+        if (filter.getHasDiscount() != null) {
+            if (filter.getHasDiscount()) {
+                query.addCriteria(AggregationExpressionCriteria.whereExpr(
+                        ComparisonOperators.valueOf("$salePrice")
+                                .lessThan("$regularPrice")
+                ));
+            } else {
+                query.addCriteria(AggregationExpressionCriteria.whereExpr(
+                        ComparisonOperators.valueOf("$salePrice")
+                                .equalTo("$regularPrice")
+                ));
+            }
+        }
+
+        return query;
     }
 }
