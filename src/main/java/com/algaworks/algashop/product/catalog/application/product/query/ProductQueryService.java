ad787c29456eb033705a06604554b57c1be61b47
Breno Pedro Assis Tenório Pinto <brenopedro14@hotmail.com>
2026-07-22 22:29:49 -0300

    feat: integração de cache entre ordering e product-catalog

--- src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductQueryService.java
+++ src/main/java/com/algaworks/algashop/product/catalog/application/product/query/ProductQueryService.java
@@ -1,9 +1,11 @@
 package com.algaworks.algashop.product.catalog.application.product.query;
 
+import org.springframework.cache.annotation.Cacheable;
+
 import java.util.UUID;
 
 public interface ProductQueryService {
+    @Cacheable(cacheNames = "algashop:products:v1", key = "#productId")
     ProductDetailOutput findById(UUID productId);
-
     PageModel<ProductSummaryOutput> filter(ProductFilter filter);
 }
